46d7db51a8377d1b51bfe5c6e8f881a8834de1bd
Pratiksha <gosavipratiksha100@gmail.com>
2025-06-27 21:51:28 +0530

    Add Day 1 CPP rpograms

---
+++ Day1/sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 20;
+    int sum = a + b;
+
+    cout << "Sum of " << a << " and " << b << " is: " << sum << endl;
+    return 0;
+}
